b9bce4a0fd625cad675698c8f31b5e755f0dd23a
james <james.mai5@hotmail.com>
2021-09-19 22:14:10 +1000

    a

--- init.lua
+++ init.lua
@@ -1,1 +1,14 @@
+require('plugins')
+require('python-lsp')
+require('clangd-lsp')
+vim.g.onedark_transparent_background = true
+require('onedark').setup()
 
+require('lualine').setup {
+    options = {
+    theme = 'onedark'
+    }
+}
+
+require('settings')
+require('nv-compe')
